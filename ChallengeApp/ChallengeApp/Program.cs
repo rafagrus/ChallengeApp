a266b3af262435be55d56756e8dddf9f92b2d739
rafagrus <rafagrus@gmail.com>
2023-02-12 16:11:48 +0100

    Tworzenie pustej aplikacji

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Rafal");
-Console.WriteLine("Cześć, Jak minął dzień ?");
+Console.WriteLine("Cześć, Jak Ci minął dzień ?");
